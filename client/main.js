ad09f87e60ed9af2af0b325d44eb24a373e20f19
Pierre PIRONIN <pierre@pierrepironin.fr>
2016-06-07 20:39:31 +0200

    Fix angular compilation

--- client/main.js
+++ client/main.js
@@ -1,7 +1,11 @@
 import angular from 'angular';
+import 'angular-component';
 import angularMeteor from 'angular-meteor';
+import userProfile from '../imports/components/userProfile/userProfile';
 
 angular.module('myway', [
     angularMeteor,
-    ngMaterial
+    userProfile.name
 ]);
+
+
