5d9891f2f4e7e6dc73c2640cc68fe175f2c59cbb
Pierre PIRONIN <pierre@pierrepironin.fr>
2016-06-08 10:43:03 +0200

    Refactor style

--- imports/components/history/history.js
+++ imports/components/history/history.js
@@ -17,10 +17,30 @@ class HistoryCtrl {
                 safety: 4,
                 comfort: 4
             },
-            { date: "10/05/2016", distance: 204.8, ecoScore: 9 },
-            { date: "09/05/2016", distance: 404, ecoScore: 8.4 },
-            { date: "08/05/2016", distance: 12.3, ecoScore: 4.6 },
-            { date: "24/04/2016", distance: 25.6, ecoScore: 6 }
+            {
+                date: "10/05/2016", distance: 204.8, ecoScore: 9, center: { latitude: 45.778570, longitude: 3.120654 },
+                consommation: 9.5,
+                safety: 8,
+                comfort: 7
+            },
+            {
+                date: "09/05/2016", distance: 404, ecoScore: 8.4, center: { latitude: 45.778570, longitude: 3.120654 },
+                consommation: 7.2,
+                safety: 9.3,
+                comfort: 5
+            },
+            {
+                date: "08/05/2016", distance: 12.3, ecoScore: 4.6, center: { latitude: 45.778570, longitude: 3.120654 },
+                consommation: 1,
+                safety: 4,
+                comfort: 4
+            },
+            {
+                date: "24/04/2016", distance: 25.6, ecoScore: 6, center: { latitude: 45.778570, longitude: 3.120654 },
+                consommation: 9,
+                safety: 4,
+                comfort: 7
+            }
         ];
         this.currentTravel = this.travels[0];
     }
