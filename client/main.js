45d832ca2406773bf13aa4954c2d900cdfccdb20
Pierre PIRONIN <pierre@pierrepironin.fr>
2016-06-08 12:07:35 +0200

    Add plus button and refactor for user profile.

--- client/main.js
+++ client/main.js
@@ -2,14 +2,10 @@ import angular from 'angular';
 import 'angular-component';
 import angularMeteor from 'angular-meteor';
 
-import history from '../imports/components/history/history';
-import userProfile from '../imports/components/userProfile/userProfile';
 import navbar from '../imports/components/navbar/navbar';
 
 angular.module('myway', [
     angularMeteor,
-    userProfile.name,
-    history.name,
     navbar.name
 ]);
 
--- imports/components/navbar/navbar.js
+++ imports/components/navbar/navbar.js
@@ -1,16 +1,29 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import history from '../history/history';
+import userProfile from '../userProfile/userProfile';
 
 import template from './navbar.html';
 
 class NavbarCtrl {
     constructor() {
+        this.showHistory = true;
+    }
+    
+    toggleUserProfile() {
+        this.showHistory = false;
+    }
+    
+    toggleHistory() {
+        this.showHistory = true;
     }
 
 }
 
 export default angular.module('navbar', [
-    angularMeteor
+    angularMeteor,
+    userProfile.name,
+    history.name
 ]).component('navbar', {
     templateUrl: 'imports/components/navbar/navbar.html',
     controller: NavbarCtrl
